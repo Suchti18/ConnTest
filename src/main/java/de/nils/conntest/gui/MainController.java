a36487468586f63da18f4b57a36f311290018d28
Nils <78668364+Suchti18@users.noreply.github.com>
2025-07-03 22:38:06 +0200

    - Updated GUI

--- src/main/java/de/nils/conntest/gui/MainController.java
+++ src/main/java/de/nils/conntest/gui/MainController.java
@@ -1,12 +1,62 @@
 package de.nils.conntest.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 
-public class MainController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable
 {
     @FXML
+    private BorderPane clientBorderPaneBtn;
+    @FXML
+    private BorderPane serverBorderPaneBtn;
+
+    @FXML
     private TextField serverPort;
+
+    private BorderPane previousSelectedBtn;
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     *                  the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        select(serverBorderPaneBtn);
+    }
+
+    @FXML
+    public void doSelectClient()
+    {
+        select(clientBorderPaneBtn);
+    }
+
+    @FXML
+    public void doSelectServer()
+    {
+        select(serverBorderPaneBtn);
+    }
+
+    public void select(BorderPane btn)
+    {
+        if(previousSelectedBtn != null)
+        {
+            previousSelectedBtn.setStyle("");
+        }
+
+        btn.setStyle("-fx-background-color: limegreen");
+        previousSelectedBtn = btn;
+    }
 
     @FXML
     public void doConnect()
